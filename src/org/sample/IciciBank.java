8e2b6831bd31a8242afb90c1bc4f260dc45ef266
kavipriyagit <tpriya8973@gmail.com>
2020-01-30 12:20:57 +0530

    new code

---
+++ src/org/sample/IciciBank.java
@@ -0,0 +1,18 @@
+package org.sample;
+
+public class IciciBank {
+	public void saving()
+	{
+		System.out.println("6%");
+	}
+	public void deposit()
+	{
+		System.out.println("4%");
+	}
+public static void main(String[] args) {
+	IciciBank i=new IciciBank();
+	i.saving();
+	i.deposit();
+	
+}
+}
